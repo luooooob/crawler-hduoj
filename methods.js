fcbd3fa2e13f5f4995698702b9fc93fb56aa08be
ssrius <572058317@qq.com>
2016-12-31 23:49:10 +0800

    正则表达式

--- methods.js
+++ methods.js
@@ -1,69 +1,112 @@
-var request = require('request');
-var cheerio = require('cheerio');
-// var async = require('async');
-// var fs = require('fs');
+const request  = require('request')
+const Iconv = require('iconv-lite')
+const async = require('async')
 
 
+/* =================================================================
+    配置账号密码
+================================================================= */
+const username = '572058317'
+const userpass = 'password'
 
 
-username = 'username'
-userpass = 'password'
+// 部分网址常量
+const webSite   = 'http://acm.hdu.edu.cn'
+const loginUrl  = webSite + '/userloginex.php?action=login'
+const infoUrl   = webSite + '/userstatus.php?user=' + username
+const searchUrl = webSite + '/status.php?first=&pid=&user=' 
+                + username + '&lang=0&status=5'
 
+// userData,用于保存所有用户信息
+var userData = {
+    name: '',
+    rank: '',
+    submit: '',
+    accept: '',
+    solved: '',
+    codeUrls: new Array()
+}
 
-var codeUrls = new Array()
+var loginOption = {
+    url: loginUrl,
+    // encoding设null，供iconv-lite模块转码
+    encoding: null,
+    formData: {
+        username: username,
+        userpass: userpass,
+        login: 'Sign In'
+    }
+}
 
-
-var signedOption = {
-    set: function() {
-      this.url = 'http://acm.hdu.edu.cn/userstatus.php?user=' + username,   
+var onlineOption = {
+    encoding: null,
+    setCookie: function(session) {   
       this.headers = {cookie: session}
     }
 }
 
-
-var loginOption = {
-    set: function() {
-        this.url = 'http://acm.hdu.edu.cn/userloginex.php?action=login'
-        this.formData = {
-            username: username,
-            userpass: userpass,
-            login: 'Sign In'
+/* =================================================================
+    在loginOption设置formData去post登录地址,返回的response.headers中
+    有个'set-cookie'属性, 将这个属性的值直接设为后面所有网址请求的cookie，
+    后面的请求就有这个账号的登录状态
+================================================================= */
+const login = function(callback) {
+    request.post(loginOption, function(error, response, body) {
+        if (!error) {
+            var session = response.headers['set-cookie']
+            onlineOption.setCookie(session)
+            callback()
         }
-    }
+    })
 }
 
 
-var login = function(callback) {
-    loginOption.set()
-    request.post(loginOption, 
-    function(error, response, body) {
-        if (error) {
-            console.log(error)
+const requestUserInfo = function() {
+    onlineOption.url = infoUrl
+    request(onlineOption, function(error, response, body) {
+        if(!error&&/action=logout/g.test(body)){
+            //用iconv-lite模块将gb2312转码成utf-8
+            body = Iconv.decode(body, 'gb2312').toString()
+            console.log('登录成功')
+            var name = body.match(/>.*(?=<\/h1>)/g)[0].substr(1)
+            var infoList = body.match(/\d+(?=<\/td><\/tr>)/g)
+            userData = {
+                name: name,
+                rank: infoList[0],
+                submit: infoList[3],
+                accept: infoList[4],
+                solved: infoList[2]
+            } 
         } else {
-            session = response.headers['set-cookie']
-            signedOption.set()
-            return callback()
+            console.log('登录失败, 请检查账号密码')
         }
-    });
+    })
 }
 
-
-var getProblemUrls = function() {
-    request(signedOption,
+const getProblemUrls = function() {
+    if(!userData.getProblemUrls) {
+        onlineOption.url = searchUrl
+    }
+    request(onlineOption, 
     function(error, response, body) {
-        if(!error){
-            $ = cheerio.load(body);
-            if($('img[alt = "Sign Out"]').length) {
-                console.log('signing')
-            }
-            if($('input[value = "Sign In"]').length) {
-                console.log('no sign')
+        if(!error) {
+            body = Iconv.decode(body, 'gb2312').toString()
+            var codeUrls = body.match(/\/viewcode\.php\?rid=\d+/g)
+            var next = body.match(/\/status\.php\?first=.*(?=">Next)/g)[0]
+            if(codeUrls) {
+                userData.codeUrls += codeUrls
+                codeUrls = null
+                onlineOption.url = next
+                getProblemUrls()
+            } else {
+                console.log(userData.codeUrls)
+                return
             }
         }
     })
 }
 
-// var starts = login(getProblemUrls)
 exports.start = function() {
     login(getProblemUrls)
+    console.log(userData.codeUrls)
 }
--- start.js
+++ start.js
@@ -1,1 +1,5 @@
+var methods = require ('./methods.js')
+
+
+methods.start()
 
